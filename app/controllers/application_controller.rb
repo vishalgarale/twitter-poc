cb068a71f28284641ba682c911cb77da827ab4a8
Vishal Garale <garale.vishal@outlook.com>
2016-06-11 15:10:13 +0530

    Update application_controller.rb
    alignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,23 +6,22 @@ class ApplicationController < ActionController::Base
   before_action :set_twitter, if: :twitter? 
 
   def after_sign_in_path_for(resource)
-	 	session[:twitter] = false
-	  root_url
+	session[:twitter] = false
+	root_url
   end
 
   def set_twitter
-  	binding.pry
   	@twitter = Twitter::REST::Client.new do |config|
-		  config.consumer_key = CONSUMER_KEY
-		  config.consumer_secret = CONSUMER_SECRET
-		  config.access_token = current_user.access_token
-		  config.access_token_secret = current_user.access_token_secret
-		end
-		current_user.tweets.destroy_all
-		@twitter.home_timeline.each do |tweet|
-			current_user.tweets.create(message: tweet.full_text, url: tweet.uri)
-    end
-		session[:twitter] = true
+	  config.consumer_key = CONSUMER_KEY
+	  config.consumer_secret = CONSUMER_SECRET
+	  config.access_token = current_user.access_token
+	  config.access_token_secret = current_user.access_token_secret
+	end
+	current_user.tweets.destroy_all
+	@twitter.home_timeline.each do |tweet|
+	  current_user.tweets.create(message: tweet.full_text, url: tweet.uri)
+        end
+	session[:twitter] = true
   end
 
   def twitter?
